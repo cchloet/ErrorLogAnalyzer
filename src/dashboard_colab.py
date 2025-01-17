8cb17dd549ca0af32f54e28799f7ad0be9080c1f
Christina Torres <38227182+cchloet@users.noreply.github.com>
2025-01-17 14:55:07 -0500

    Update dashboard_colab.py

--- src/dashboard_colab.py
+++ src/dashboard_colab.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import plotly.express as px
-#from google.colab import files
-#import os
 import re
 from collections import Counter
+import os
 
-def source_data_length(file_path = 'data/raw/corrupted_retail_sales_dataset.csv'):
+def source_data_length(file_path):
     data = pd.read_csv(file_path)
     return len(data)
 
@@ -24,13 +23,14 @@ def summarize_errors(log_file_path):
             for error_type, pattern in error_patterns.items():
                 if pattern.search(line):
                     error_counts[error_type] += 1
-    
-    valid_rows = source_data_length()
+    cwd = os.getcwd()
+    file_path = 'cwd'+'/data/corrupted_retail_sales_dataset.csv'
+    valid_rows = source_data_length(file_path)
     error_counts['Valid Data'] = valid_rows
     
     return error_counts
 
-def visualize_error_logs(log_file_path='logs/cleaning_errors.log', save_path='dashboard/error_summary.html'):
+def visualize_error_logs(log_file_path):
     # Summarize the errors using the summarize_errors function
     error_summary = summarize_errors(log_file_path)
     
@@ -41,9 +41,12 @@ def visualize_error_logs(log_file_path='logs/cleaning_errors.log', save_path='da
     fig = px.pie(error_summary_df, names='Error Type', values='Count', title='Error Summary')
     
     # Save the interactive chart to an HTML file
+    cwd = os.getcwd()
+    save_path = 'cwd'+'dashboard/error_summary.html'
     fig.write_html(save_path)
-    fig.show()
 
 # Run the visualization
 if __name__ == "__main__":
-    visualize_error_logs()
+    cwd = os.getcwd()
+    file_path = 'cwd'+'logs/cleaning_errors.log'
+    visualize_error_logs(file_path)
